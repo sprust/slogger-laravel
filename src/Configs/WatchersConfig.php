d3f3461ae31e0e544978e39b7bbc9e29cb8c5781
spp28 <sppetrovich@mail.ru>
2026-02-22 09:28:53 +0200

    remove unused methods in WatchersConfig

--- src/Configs/WatchersConfig.php
+++ src/Configs/WatchersConfig.php
@@ -15,42 +15,10 @@ readonly class WatchersConfig
     }
 
     /**
-     * @return array<string, string[]>
-     */
-    public function modelsMasks(): array
-    {
-        return config('slogger.watchers_customizing.models.masks') ?? [];
-    }
-
-    /**
      * @return string[]
      */
     public function getDataCompleterExcludedFileMasks(): array
     {
         return config('slogger.data_completer.excluded_file_masks') ?? [];
-    }
-
-    /**
-     * @return class-string<object>[]
-     */
-    public function eventsIgnoreEvents(): array
-    {
-        return config('slogger.watchers_customizing.events.ignore_events');
-    }
-
-    /**
-     * @return class-string<object>[]
-     */
-    public function eventsSerializeEvents(): array
-    {
-        return config('slogger.watchers_customizing.events.serialize_events');
-    }
-
-    /**
-     * @return class-string<object>[]
-     */
-    public function eventsCanBeOrphan(): array
-    {
-        return config('slogger.watchers_customizing.events.can_be_orphan');
     }
 }
