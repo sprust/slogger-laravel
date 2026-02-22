58d161b1549c2938563ebcbb8b13d4dc5c4e6791
spp28 <sppetrovich@mail.ru>
2026-02-22 10:31:53 +0200

    Add DatabaseWatcher tests: implement DatabaseTest with successCallback and trace assertions

---
+++ tests/Feature/Watchers/Children/Database/DatabaseTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SLoggerLaravel\Tests\Feature\Watchers\Children\Database;
+
+use Closure;
+use Illuminate\Support\Facades\DB;
+use SLoggerLaravel\Objects\TraceCreateObject;
+use SLoggerLaravel\Objects\TraceUpdateObject;
+use SLoggerLaravel\Tests\Feature\Watchers\Children\BaseChildrenTestCase;
+use SLoggerLaravel\Watchers\Children\DatabaseWatcher;
+
+class DatabaseTest extends BaseChildrenTestCase
+{
+    protected function getTraceType(): string
+    {
+        return 'database';
+    }
+
+    protected function getWatcherClass(): string
+    {
+        return DatabaseWatcher::class;
+    }
+
+    protected function successCallback(): Closure
+    {
+        return fn() => event(
+            DB::statement('SELECT 1')
+        );
+    }
+
+    protected function assertSuccess(
+        TraceCreateObject $creatingTrace,
+        TraceUpdateObject $updatingTrace
+    ): void {
+        // no action
+    }
+}
